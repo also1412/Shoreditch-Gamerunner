26a1fc998ef3c2a922e3d4949bb5c58defe93685
Jonathan Scott <jonathanscott1989@gmail.com>
2012-04-18 11:11:31 +0100

    Can force player id

--- test/test_admin.py
+++ test/test_admin.py
@@ -34,3 +34,17 @@ class AdminTest(unittest.TestCase):
 
 		response, data = request('player', 'POST', player_data)
 		assert response.status == 400, (response, data)
+
+	def test_register_with_id(self):
+		player_data = {
+			"id": uuid4().hex,
+			"name": uuid4().hex,
+			"endpoint": "http://example.com/"
+		}
+
+		response, data = request('player', 'POST', player_data)
+		assert response.status == 201, (response, data)
+
+		player = json.loads(data)['player']
+
+		assert player['id'] == player_data['id'], (response, data)
